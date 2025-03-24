60f7d98b46c380c01e96ce1066f97b69e468b26b
Mahesh Pulivarthi <pulivarthimahesh1@gmail.com>
2025-03-24 14:53:26 +0530

    inserted new user record into users collection

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const { default: mongoose } = require("mongoose");
 const { connectDB } = require("./config/database");
+const User = require("./models/user");
 
 const app = express();
+
+app.use("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Mahesh",
+  });
+  await user.save();
+  res.send("User added successfully");
+});
 
 connectDB()
   .then(() => {
