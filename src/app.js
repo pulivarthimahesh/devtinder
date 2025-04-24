bd49386329f4e840d032d8b59e97cc170ef07774
Mahesh Pulivarthi <pulivarthimahesh1@gmail.com>
2025-04-24 18:44:04 +0530

    Post and get users API are done

--- src/app.js
+++ src/app.js
@@ -5,12 +5,34 @@ const User = require("./models/user");
 
 const app = express();
 
-app.use("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Mahesh",
-  });
-  await user.save();
-  res.send("User added successfully");
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.send("User added successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong !!! " + err.message);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  try {
+    const users = await User.find({ emailId: req.body.emailId });
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong!!! " + err.message);
+  }
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong!!! " + err.message);
+  }
 });
 
 connectDB()
--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   firstName: String,
+  lastName: String,
+  emailId: String,
+  password: String,
+  age: Number,
 });
 
 const userModel = mongoose.model("user", userSchema);
