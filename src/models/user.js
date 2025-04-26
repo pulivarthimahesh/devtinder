d3f34c69dbf424e4abfc78152d8b608b814d8d6a
Mahesh Pulivarthi <pulivarthimahesh1@gmail.com>
2025-04-26 19:44:51 +0530

    Explored Validator package and implemented in user model.

--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,38 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
     minLength: 4,
     required: true,
+    trim: true,
   },
   lastName: {
     type: String,
     minLength: 4,
     required: true,
+    trim: true,
   },
   emailId: {
     type: String,
     required: true,
     unique: true,
+    trim: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Entered email is not valid " + value);
+      }
+    },
   },
   password: {
     type: String,
     required: true,
+    validate(value) {
+      if (!validator.isStrongPassword(value)) {
+        throw new Error("Entered password is not strong " + value);
+      }
+    },
   },
   age: {
     type: Number,
