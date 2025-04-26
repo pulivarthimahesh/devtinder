fe5ac3ccdea0f7a1bc50490732882659bdab4304
Mahesh Pulivarthi <pulivarthimahesh1@gmail.com>
2025-04-26 18:47:12 +0530

    Added validations for the user form

--- src/models/user.js
+++ src/models/user.js
@@ -1,11 +1,29 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  emailId: String,
-  password: String,
-  age: Number,
+  firstName: {
+    type: String,
+    minLength: 4,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    minLength: 4,
+    required: true,
+  },
+  emailId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    min: 18,
+  },
 });
 
 const userModel = mongoose.model("user", userSchema);
